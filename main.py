e9004ba50a11ef0ea09b6fa24f0e7f5efe4f3844
Tishues <BradleyBounds@outlook.com>
2025-01-05 19:02:31 -0600

    D: adding get_lettercount function

--- main.py
+++ main.py
@@ -2,14 +2,25 @@ def main():
     bookpath = "books/frankenstein.txt"
     text = get_booktext(bookpath)
     wordcount = get_wordcount(text)
+    lettercounts = get_lettercount(text)
     print(f"Word count: {wordcount}")
 
 def get_booktext(path):
     with open(path) as f:
         return f.read()
     
-def get_wordcount(text):
-    words = text.split()
+def get_wordcount(book):
+    words = book.split()
     return len(words)
+
+def get_lettercount(text):
+    text = text.lower()
+    lettercount = {}
+    for letter in text:
+        if letter in lettercount:
+            lettercount[letter] += 1
+        else:
+            lettercount[letter] = 1
+    return lettercount
 
 main()
